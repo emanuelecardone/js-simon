ea627d3b45114ce8e3d157da92db6c42ef575c4a
emanuelecardone <e_m.cardone@yahoo.it>
2021-11-30 19:07:09 +0100

    Fatto

--- Bonus - eventi start-stop/js/script.js
+++ Bonus - eventi start-stop/js/script.js
@@ -4,25 +4,31 @@ const pageBody = document.querySelector('body');
 // Classi stile body
 pageBody.classList.add('w-100', 'vh-100', 'bg-dark');
 
-// Variabile header, tasti start/stop, main, box container, set fluid+row+cols e footer
+// Variabile header, tasti start/stop, main, colors container, box container, set fluid+row+cols e footer
 const pageHeader = document.createElement('header');
 const startButton = document.createElement('button');
 const stopButton = document.createElement('button');
 
 const pageMain = document.createElement('main');
+const colorsContainer = document.createElement('div');
+const colorsFluid = document.createElement('div');
+const colorsRow = document.createElement('div');
 const boxContainer = document.createElement('div');
 const boxFluid = document.createElement('div');
 const boxRow = document.createElement('div');
 
 const pageFooter = document.createElement('footer');
 
-// Classi stile header, tasti start/stop, main, box container, set fluid+row+cols e footer
+// Classi stile header, tasti start/stop, main, colors container, box container, set fluid+row+cols e footer
 // Il position fixed è un debug per non far muovere il bottone deselezionato all'animazione del selezionato
 pageHeader.classList.add('w-100', 'h_100p', 'd-flex', 'justify-content-center', 'align-items-center');
 startButton.classList.add('start_btn', 'btn', 'btn-danger', 'text-white', 'fw-bold', 'text-center', 'text-uppercase', 'position-fixed');
 stopButton.classList.add('stop_btn', 'btn', 'btn-danger', 'text-white', 'fw-bold', 'text-center', 'text-uppercase', 'position-fixed');
 
-pageMain.classList.add('w-100', 'border', 'd-flex', 'justify-content-center', 'align-items-center', 'border-5', 'border-danger');
+pageMain.classList.add('w-100', 'border', 'd-flex', 'justify-content-center', 'align-items-center', 'border-5', 'border-danger', 'position-relative');
+colorsContainer.classList.add('colors_wrapper', 'w_500p', 'h_75p', 'position-absolute');
+colorsFluid.classList.add('container-fluid', 'h-100');
+colorsRow.classList.add('row', 'row-cols-4', 'h-100');
 boxContainer.classList.add('w_500p', 'h_500p');
 boxFluid.classList.add('container-fluid', 'h-100');
 boxRow.classList.add('row', 'row-cols-5', 'h-100');
@@ -33,21 +39,67 @@ pageFooter.classList.add('w-100', 'h_100p');
 startButton.innerText = 'Start';
 stopButton.innerText = 'Stop';
 
-// Inserimento in pagina header, tasti start/stop, main, box container, set fluid+row+cols e footer
+// Inserimento in pagina header, tasti start/stop, main, colors container, colors, box container, set fluid+row+cols e footer
 pageBody.append(pageHeader, pageMain, pageFooter);
 
 pageHeader.append(startButton, stopButton);
 
-pageMain.appendChild(boxContainer);
+pageMain.append(colorsContainer, boxContainer);
+colorsContainer.appendChild(colorsFluid);
+colorsFluid.appendChild(colorsRow);
+// Numero di colori che voglio
+// Aggiungo un'animazione bordi per il pulsante creato (uso la col invece di un pulsante effettivo)
+// il pulsante o col rimuoverà anche l'effetto bordi agli altri
+// Faccio poi una variabile che tiene conto del colore selezionato (mi serve per decidere il colore da dare alle col)
+// Questa variabile avrà cyan di default dato che è selected, ma cambierà al click di colori diversi
+// L'obiettivo è far sì che l'utente possa cambiare colore anche mentre le col si stanno riempendo
+// senza il bisogno di clickare stop e poi cambiare
+const colorsNumber = 4;
+let userColor = 'cyan';
+for(let i = 0; i < colorsNumber; i++){
+
+    const currentColorsCol = document.createElement('div');
+    currentColorsCol.classList.add('col', 'colors_col');
+    currentColorsCol.addEventListener('click', function(){
+        for(let j = 0; j < colorsNumber; j++){
+            document.getElementsByClassName('colors_col')[j].classList.remove('border_ani');
+        }
+        this.classList.add('border_ani');
+    });
+
+    switch(i){
+        case 0:
+            // Effetto selected di default per il primo
+            currentColorsCol.classList.add('cyan_bg', 'border_ani');
+            currentColorsCol.addEventListener('click', function(){userColor = 'cyan'});
+            break;
+        case 1:
+            currentColorsCol.classList.add('red_bg');
+            currentColorsCol.addEventListener('click', function(){userColor = 'red'});
+            break;
+        case 2:
+            currentColorsCol.classList.add('yellow_bg');
+            currentColorsCol.addEventListener('click', function(){userColor = 'yellow'});
+            break;
+        case 3:
+            currentColorsCol.classList.add('purple_bg');
+            currentColorsCol.addEventListener('click', function(){userColor = 'purple'});
+            break;           
+    }
+   
+    colorsRow.appendChild(currentColorsCol);
+}
 boxContainer.appendChild(boxFluid);
 boxFluid.appendChild(boxRow);
 // Numero di box che voglio
 const boxesNumber = 25;
 for(let i = 0; i < boxesNumber; i++){
     const currentBoxCol = document.createElement('div');
-    currentBoxCol.classList.add('col','border', 'border-3', 'border-danger', 'bg-white');
+    currentBoxCol.classList.add('col', 'boxes_col', 'border', 'border-3', 'border-danger', 'bg-white');
     boxRow.appendChild(currentBoxCol);
 }
+
+
 
 
 // Devo dichiarare boxClocks fuori per renderlo leggibile da Start e Stop
@@ -86,8 +138,8 @@ startButton.addEventListener('click', function(){
     boxesClock = setInterval(function(){
 
         // Devo necessariamente selezionare le col in questo modo altrimenti lavorerei su un array copiato
-        document.getElementsByClassName('col')[myCounter].classList.remove('bg-white');
-        document.getElementsByClassName('col')[myCounter].classList.add('bg-info');
+        document.getElementsByClassName('boxes_col')[myCounter].classList.remove('bg-white');
+        document.getElementsByClassName('boxes_col')[myCounter].classList.add(`${userColor}_bg`);
 
         // Aumenta il counter
         myCounter++;
@@ -103,8 +155,8 @@ startButton.addEventListener('click', function(){
                 // Dopo 1 secondo dalla fine dell'evento, il colore delle col si ripristina
                 // così da poter ricominciare l'evento colorandole come prima
                 const resetClock = setTimeout(function(){
-                    document.getElementsByClassName('col')[i].classList.remove('bg-info');
-                    document.getElementsByClassName('col')[i].classList.add('bg-white');
+                    document.getElementsByClassName('boxes_col')[i].classList.remove(`${userColor}_bg`);
+                    document.getElementsByClassName('boxes_col')[i].classList.add('bg-white');
                     // Rimuovo l'animazione dal tasto start poiché non sarà più attivo
                     startButton.classList.remove('border_ani');
                 }, 1000);
